0cdc4b320d0594f23b5d6412029311a2f036361a
Riyaz Pathan <moh.riyazpathan@gmail.com>
2025-12-20 11:51:05 +0530

    Added images related to Type Change

--- 01 Basics/04 First Program with Browser/src/index.ts
+++ 01 Basics/04 First Program with Browser/src/index.ts
@@ -4,3 +4,9 @@ function greet(name: string): string {
 
 const user: string = "Riyaz";
 console.log(greet(user));
+
+// The last line will cause a TypeScript error because '1' is not a string.
+// To fix the error, we should pass a string argument to the greet function.
+// console.log(greet("1"));
+// console.log(greet(1));
+
