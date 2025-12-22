bc738d845eb4a757b0d6e619d0fdc629f186458e
Riyaz Pathan <moh.riyazpathan@gmail.com>
2025-12-22 16:13:59 +0530

    Added Details about Number DataType

---
+++ 01 Basics/07 Number DataType/number.js
@@ -0,0 +1,9 @@
+// tsc index.js --watch
+// npx tsc index.js --watch
+// var variable_name: data_type = value;
+var num1 = 10;
+var num2 = 20;
+var num3 = 30;
+// var sum = num1 + num2;
+var sum = num1 + num2 + num3;
+console.log("Sum is: " + sum);
